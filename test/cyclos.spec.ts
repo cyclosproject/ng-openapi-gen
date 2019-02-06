88dc3ac0f36a808d0d99ae57535eff1499fbe551
Luis Fernando Planella Gonzalez <luisfpg@cyclos.org>
2019-02-06 10:22:02 -0200

    Fixed a compilation error

--- test/cyclos.spec.ts
+++ test/cyclos.spec.ts
@@ -2,8 +2,9 @@ import { OpenAPIObject } from 'openapi3-ts';
 import { NgOpenApiGen } from '../lib/ng-openapi-gen';
 import options from './cyclos.config.json';
 import allOperationsSpec from './cyclos.json';
+import { Options } from '../lib/options';
 
-const gen = new NgOpenApiGen(allOperationsSpec as OpenAPIObject, options);
+const gen = new NgOpenApiGen(allOperationsSpec as OpenAPIObject, options as Options);
 gen.generate();
 
 describe('Generation tests using cyclos.json', () => {
