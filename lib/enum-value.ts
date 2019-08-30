a334c924a50cd0f585af05eeac6a62f54a4dc01e
tim-elektron-fo <54707242+tim-elektron-fo@users.noreply.github.com>
2019-08-30 11:08:45 +0100

    Handle x-enumNames

--- lib/enum-value.ts
+++ lib/enum-value.ts
@@ -9,9 +9,9 @@ export class EnumValue {
   name: string;
   value: string;
 
-  constructor(public type: string, _value: any, public options: Options) {
+  constructor(public type: string, _value: any, public options: Options, public name: string) {
     const value = String(_value);
-    this.name = enumName(value, options);
+    this.name = enumName(name || value, options);
     if (type === 'string') {
       this.value = `'${jsesc(value)}'`;
     } else {
