7d48732e542bf9b0bf1f358ca489beec56a86420
Luis Fernando Planella Gonzalez <lfpg.dev@gmail.com>
2020-03-02 08:17:47 -0300

    Changed the version to 0.8.1 and updated deps

--- test/cyclos.spec.ts
+++ test/cyclos.spec.ts
@@ -1,10 +1,10 @@
 import { OpenAPIObject } from 'openapi3-ts';
 import { NgOpenApiGen } from '../lib/ng-openapi-gen';
 import options from './cyclos.config.json';
-import allOperationsSpec from './cyclos.json';
+import cyclosSpec from './cyclos.json';
 import { Options } from '../lib/options';
 
-const gen = new NgOpenApiGen(allOperationsSpec as OpenAPIObject, options as Options);
+const gen = new NgOpenApiGen(cyclosSpec as any as OpenAPIObject, options as Options);
 gen.generate();
 
 describe('Generation tests using cyclos.json', () => {
