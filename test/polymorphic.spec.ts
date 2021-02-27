d75c48d1b7526893a76eb943f8334d161449b3fd
Erik Konijnenburg <erik.konijnenburg@devops.ncia.nato.int>
2021-02-27 14:39:55 +0100

    Cleanup spec

--- test/polymorphic.spec.ts
+++ test/polymorphic.spec.ts
@@ -21,33 +21,4 @@ describe("Generation of derived classes using polymorphic.json (as is generated 
     });
     done();
   });
-  // const baz = gen.models.get("Foo.Bar.Baz");
-  // const ts = gen.templates.apply("model", baz);
-  // const parser = new TypescriptParser();
-  // parser.parseSource(ts).then((ast) => {
-  //   expect(ast.declarations.length).toBe(1);
-  //   expect(ast.declarations[0]).toEqual(jasmine.any(InterfaceDeclaration));
-  //   const decl = ast.declarations[0] as InterfaceDeclaration;
-  //   expect(decl.name).toBe("Baz");
-  //   expect(decl.properties.length).toBe(3);
-  //   const ref = decl.properties.find((p) => p.name === "refProperty");
-  //   expect(ref).withContext("refProperty property").toBeDefined();
-  //   if (ref) {
-  //     expect(ref.type).toBe("Baz");
-  //   }
-  //   const array = decl.properties.find((p) => p.name === "arrayProperty");
-  //   expect(array).withContext("arrayProperty property").toBeDefined();
-  //   if (array) {
-  //     expect(array.type).toBe("Array<Baz>");
-  //   }
-  //   const object = decl.properties.find((p) => p.name === "objectProperty");
-  //   expect(object).withContext("objectProperty property").toBeDefined();
-  //   if (object) {
-  //     expect(object.type).toBe(
-  //       `{ 'nestedArray': Array<Baz>, 'nestedRef': Baz }`
-  //     );
-  //  }
-  //   done();
-  // });
-  //});
 });
